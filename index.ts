5f99c37222feb495565eca0dc712c72de1461e3e
Hakim <sio.makhtour@gmail.com>
2018-10-26 11:53:58 +0200

    commit test

--- index.ts
+++ index.ts
@@ -4,34 +4,41 @@
  * Module dependencies.
  */
 
-import commander from 'commander';
+// import commander from 'commander';
+// import nodegit from 'nodegit';
+// import path from 'path';
+import fs from 'fs';
+
+// var url = "https://github.com/amfyWebService/AMFY-CLI",
+//     local = "./AMFY-CLI",
+//     cloneOpts = {};
 
 // commander
 //   .version('0.1.0')
-//   .option('-p, --peppers', 'Add peppers')
-//   .option('-P, --pineapple', 'Add pineapple')
-//   .option('-b, --bbq-sauce', 'Add bbq sauce')
-//   .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
-//   .option('--no-sauce', 'Remove sauce')
+//   .option('init, --init', 'start')
 //   .parse(process.argv);
 
-// console.log('you ordered a pizza :');
-// if (commander.peppers) console.log('  - peppers');
-// if (commander.pineapple) console.log('  - pineapple');
-// if (commander.bbqSauce) console.log('  - bbq');
-// console.log('  - %s cheese', commander.cheese);
-// if (commander.sauce) console.log('  - with sauce');
-// else console.log('  - without sauce');
+// if (commander.init){
+//   nodegit.Clone.clone(url, local, cloneOpts).then(function (repo) {
+//     console.log("Cloned " + path.basename(url) + " to " + repo.workdir());
+//   }).catch(function (err) {
+//     console.log(err);
+//     process.exit();
+//   });
+// } 
 
+// function renameFile(oldFile: string, newName: string): void {
 
-// --------------------------------------
+//   fs.rename(oldpath, newPath, (err) => {
+//     if (err) throw err;
+//     console.log('renamed complete');
+//   });
+// }
 
+// renameFile('./AMFY-CLI/index.ts','./AMFY-CLI/toto.ts');
 
-// commander
-//   .version('0.1.0')
-//   .option('-s --size <size>', 'Pizza size', /^(large|medium|small)$/i, 'medium')
-//   .option('-d --drink [drink]', 'Drink', /^(coke|pepsi|izze)$/i)
-//   .parse(process.argv);
-
-// console.log(' size: %j', commander.size);
-// console.log(' drink: %j', commander.drink);
+// const oldFile = path.substring(path.lastIndexOf("/")+1, path.length); 
+// fs.rename('./AMFY-CLI/toto.ts', './AMFY-CLI/index.ts', (err) => {
+//   if (err) throw err;
+//   console.log('renamed complete');
+// });
