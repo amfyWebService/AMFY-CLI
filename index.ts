561265d1ba032a77c3197b4f2ac9c3f148b37aee
Hakim <sio.makhtour@gmail.com>
2018-10-26 09:26:03 +0200

    start index.ts with commander

---
+++ index.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+import commander from 'commander';
+
+// commander
+//   .version('0.1.0')
+//   .option('-p, --peppers', 'Add peppers')
+//   .option('-P, --pineapple', 'Add pineapple')
+//   .option('-b, --bbq-sauce', 'Add bbq sauce')
+//   .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+//   .option('--no-sauce', 'Remove sauce')
+//   .parse(process.argv);
+
+// console.log('you ordered a pizza :');
+// if (commander.peppers) console.log('  - peppers');
+// if (commander.pineapple) console.log('  - pineapple');
+// if (commander.bbqSauce) console.log('  - bbq');
+// console.log('  - %s cheese', commander.cheese);
+// if (commander.sauce) console.log('  - with sauce');
+// else console.log('  - without sauce');
+
+
+// --------------------------------------
+
+
+// commander
+//   .version('0.1.0')
+//   .option('-s --size <size>', 'Pizza size', /^(large|medium|small)$/i, 'medium')
+//   .option('-d --drink [drink]', 'Drink', /^(coke|pepsi|izze)$/i)
+//   .parse(process.argv);
+
+// console.log(' size: %j', commander.size);
+// console.log(' drink: %j', commander.drink);
