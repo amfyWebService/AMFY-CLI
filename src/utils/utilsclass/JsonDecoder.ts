ad608a95f7e54837539371e2fdc11636b5b763e4
Farsen AC <farsen.abdallahcombo@ynov.com>
2018-11-02 09:25:54 +0100

    fix: fix bad import and tsconfig

--- src/utils/utilsclass/JsonDecoder.ts
+++ src/utils/utilsclass/JsonDecoder.ts
@@ -1,9 +1,9 @@
-import {Decoder, object, string, optional, number, boolean} from '../../node_modules/@mojotech/json-type-validation'
 import {iChangingValues} from "./iChangingValue"
+import { Decoder, object, string } from "@mojotech/json-type-validation";
 const fs = require("fs")
 
 export const decoderValueTochange: Decoder<iChangingValues> = object({
     toReplace: string(),
-    replacer: string()
+    replacer : string()
 })
 
